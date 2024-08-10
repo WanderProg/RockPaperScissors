7306a5f8edfc5a98de4455a09e8772465a0a680d
Wander van Leeuwen <wanderleeuwenvan@gmail.com>
2024-08-10 22:35:37 +0200

    finish game

--- script.js
+++ script.js
@@ -6,39 +6,87 @@ const column1 = document.querySelector(".column1")
 const column2 = document.querySelector(".column2")
 const column3 = document.querySelector(".column3")
 
-// Write a function that appends the emoji of chosen button to column1
+const playerScore = document.querySelector(".player-score")
+const computerScore = document.querySelector(".computer-score")
+const movesLeft = document.querySelector(".moves-left")
+
+const columns = document.querySelectorAll("[data-col]")
 
 
-rock.addEventListener("click", () => handleClick("👊"))
-paper.addEventListener("click", () => handleClick("✋"))
+let playerGlobalScore = 0
+let computerGlobalScore = 0
+
+let playerCurrentScore = 0
+let computerCurrentScore = 0
+
+let currentRound = 5
+
+const choices = ["👊", "✋", "✌️"]
 
 
-function handleClick(choiceOfUser) {
-    const choiceOfComputer = getRandomEmoji();
-    appendOwnPickToDom(choiceOfUser);
-    appendComputerPickToDom(choiceOfComputer);
-    const winner = checkWinner(choiceOfUser, choiceOfComputer)
-    appendWinnerToDom(winner);
+rock.addEventListener("click", () => gameItteration(0))
+paper.addEventListener("click", () => gameItteration(1))
+scissors.addEventListener("click", () => gameItteration(2))
+
+
+function gameItteration(userInput) {
+    console.log(columns)
+    const computerChoice = Math.floor(Math.random() * choices.length)
+    if(userInput === computerChoice) {
+        domUpdater(choices[userInput],"column1");
+        domUpdater(choices[computerChoice],"column2");
+        domUpdater("Draw","column3")
+    } else if (userInput === 0 && computerChoice === 2 || userInput === 1 && computerChoice === 0 || userInput === 2 && computerChoice === 1) {
+        domUpdater(choices[userInput],"column1");
+        domUpdater(choices[computerChoice],"column2");
+        domUpdater("Player","column3")
+        playerCurrentScore++
+    } else {
+        domUpdater(choices[userInput],"column1");
+        domUpdater(choices[computerChoice],"column2");
+        domUpdater("Computer","column3")
+        computerCurrentScore++
+    }
+
+    currentRound--
+    movesLeft.innerHTML = currentRound
+
+    if(currentRound === 0) {
+        checkWinner()
+        
+    }
+
 }
 
-// function handleClick(choiceOfUser) {
-//     const userChoice = document.createElement("div")
-//     userChoice.classList.add("own-pick")
-//     userChoice.innerText = choiceOfUser
-//     column1.appendChild(userChoice)
-// }
+function checkWinner() {
+    currentRound = 5
+    movesLeft.innerHTML = currentRound
+    if (playerCurrentScore > computerCurrentScore) {
+        playerGlobalScore++
+    } else if(computerCurrentScore > playerCurrentScore) {
+        computerGlobalScore++
+    } else{
+        alert("No one wins")
+        
+    }
+    
+    columns.forEach(column => {
+        column.innerHTML = ""
+    })
+    playerCurrentScore = 0
+    computerCurrentScore = 0
 
-/*deze functie gebruiken om de keuze van jezelf aan de DOM toe te voegen.
-PARAMETERS: keuzen van gebruiker
-Returned: Niks
-*/
-
-function appendOwnPickToDom(choiceOfUser) {
-    const userChoice = document.createElement("div")
-    userChoice.classList.add("own-pick")
-    userChoice.innerText = choiceOfUser
-    column1.appendChild(userChoice)
+    playerScore.innerHTML = playerGlobalScore
+    computerScore.innerHTML = computerGlobalScore
 }
 
 
+function domUpdater(emoji, column) {
 
+    const emojiDiv = document.createElement("div")
+    emojiDiv.innerText = emoji
+
+
+    const targetColumn = document.querySelector(`.${column}`);
+    targetColumn.appendChild(emojiDiv)
+}
